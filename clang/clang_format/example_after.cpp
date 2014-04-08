013b3a55c8d3c9b34086111008dcf7988d40533c
Max Thrun <bear24rw@gmail.com>
2014-04-08 07:59:31 -0400

    clang pdf

--- clang/clang_format/example_after.cpp
+++ clang/clang_format/example_after.cpp
@@ -1,33 +1,41 @@
 void test()
 {
-   switch (x) {
-   case 0:
-   case 1:
-           do_something();
-   break;
-   case 2:
-           do_something_else();
-   break;
-   default:
-   break;
-   }
+    switch (x) {
+        case 0:
+        case 1:
+            do_something();
+            break;
+        case 2:
+            do_something_else();
+            break;
+        default:
+            break;
+    }
 
-   if (condition)
-           x++;
+    if (condition) x++;
 
-   if(x==y){
-           q();
-   } else if(x>y) {
-           w();
-   }else{
-           r();
-   }
+    if (x == y) {
+        q();
+    } else if (x > y) {
+        w();
+    } else {
+        r();
+    }
 
-   function_one(); // this is function 1
-   func(); // another function
-   func_two(); // function two
+    function_one();  // this is function 1
+    func();          // another function
+    func_two();      // function two
 
-   this_is_a_really_long_function(argument_one, argument_two, argument_three, argument_four, argument_five);
+    this_is_a_really_long_function(argument_one,
+                                   argument_two,
+                                   argument_three,
+                                   argument_four,
+                                   argument_five);
 
-    double squareness = num_nets*-1.62428552015961E-4+1.0/(grid_h*grid_h)*num_cells*3.02812966834494E-1+2.41123889653172/grid_h+pow(6.19504321450544E-1,grid_w)*grid_h-pow(6.19504321450544E-1,grid_w)*num_cells*3.02812966834494E-1+3.41290981390719E-1;
+    double squareness =
+        num_nets * -1.62428552015961E-4 +
+        1.0 / (grid_h * grid_h) * num_cells * 3.02812966834494E-1 +
+        2.41123889653172 / grid_h + pow(6.19504321450544E-1, grid_w) * grid_h -
+        pow(6.19504321450544E-1, grid_w) * num_cells * 3.02812966834494E-1 +
+        3.41290981390719E-1;
 }
